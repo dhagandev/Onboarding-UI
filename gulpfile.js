aea2f2eefe3a6b88baf273df90d87dcea9068898
dhagandev <dhagan@utexas.edu>
2018-08-16 14:04:46 -0500

    Updated the readme to reflect changes in gulp, also included npm install in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ gulp.task('hello', function() {
 	console.log("Hello World!");
 });
 
-gulp.task('dev', function(callback) {
+gulp.task('dev', function() {
+	exec('node install');
 	exec('node src/js/main.js');
 });

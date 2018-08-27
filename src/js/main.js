cf5af883b885e48b759e413971c8437126078edd
dhagandev <dhagan@utexas.edu>
2018-08-27 16:09:42 -0500

    Removed console log in main

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,4 @@
 import {run} from './reactRenderComponents.js';
-const hostname = '127.0.0.1';
-const port = 9000;
-
-console.log(`Server running at http://${hostname}:${port}/`);
 
 window.onload = () => {
     run();
